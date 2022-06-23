617eb45eb754f7e84fba95c44911b804b16945a9
amiranr <nuramira381@gmail.com>
2022-06-23 21:40:56 +0800

    try 1

--- src/filters/LowercaseTransformer.java
+++ src/filters/LowercaseTransformer.java
@@ -1,70 +1,23 @@
-package pipefilter.filter;
-
-import pipefilter.pipe.Pipe;
-
 import java.util.concurrent.CountDownLatch;
+import akka.*;
+import akka.actor.UntypedAbstractActor;
+import akka.event.LoggingAdapter;
 
-import static pipefilter.config.Configuration.SENTINEL_VALUE;
 
+public class LowercaseTransformer extends UntypedActor {
+    
+    private ActorRef nextActor;
+    private LoggingAdapter log = Logging.getLogger(getContext().System(), this);
 
-public class ToLowerCaseTransformer implements Filter<String, String> {
-
-    private final Pipe<String> input;
-    private final Pipe<String> output;
-    private final CountDownLatch doneSignal;
-
-    /**
-     * Timing instrumentation instance variables
-     */
-    private long cumulativeInputBlockingTime;
-    private long cumulativeOutputBlockingTime;
-    private long totalProcessingTime;
-    private long inputCounter;
-    private long outputCounter;
-
-    public ToLowerCaseTransformer(Pipe<String> input, Pipe<String> output, CountDownLatch doneSignal) {
-        this.input = input;
-        this.output = output;
-        this.doneSignal = doneSignal;
+    public LowercaseTransformer(ActorRef nextActor){
+        this.nextActor = nextActor;
     }
+     @Override
+     public void onReceive(Object previousmessage) throws Throwable{
+        log.info("Received"+ previousmessage);
+        String message = (String) previousmessage;
+        String output = message.toLowerCase();
+        nextActor.tell(output, getSelf());
+     }
 
-    @Override
-    public void filter() {
-        inputCounter = 0L;
-        outputCounter = 0L;
-        cumulativeInputBlockingTime = 0L;
-        cumulativeOutputBlockingTime = 0L;
-        long start = System.currentTimeMillis();
-        while(true) {
-            long beforeInputPipe;
-            long beforeOutputPipe;
-            try {
-                beforeInputPipe = System.currentTimeMillis();
-                final String word = input.take();
-                cumulativeInputBlockingTime += System.currentTimeMillis() - beforeInputPipe;
-                inputCounter++;
-
-                if(word.equals(SENTINEL_VALUE)) {
-                    beforeOutputPipe = System.currentTimeMillis();
-                    output.put(SENTINEL_VALUE);
-                    cumulativeOutputBlockingTime += System.currentTimeMillis() - beforeOutputPipe;
-                    break;
-                }
-                beforeOutputPipe = System.currentTimeMillis();
-                output.put(word.toLowerCase());
-                cumulativeOutputBlockingTime += System.currentTimeMillis() - beforeOutputPipe;
-                outputCounter++;
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-        }
-        totalProcessingTime = System.currentTimeMillis() - start;
-    }
-
-    @Override
-    public void run() {
-        filter();
-        doneSignal.countDown();
-        System.out.printf("%1$-26s | %2$10s | %3$11s | %4$9s | %5$8s | %6$8s%n", getClass().getSimpleName(), cumulativeInputBlockingTime, cumulativeOutputBlockingTime, totalProcessingTime, inputCounter, outputCounter);
-    }
-}
+} 
